8449e790d082a2fe657a6ebf0355ad1dc4f289ea
DistrictDigitalAgency <47350458+DistrictDigitalAgency@users.noreply.github.com>
2019-02-28 19:24:32 +0100

    v28022019

--- app/src/main/java/districtdigital/agency/votingPackage/MainFragment.java
+++ app/src/main/java/districtdigital/agency/votingPackage/MainFragment.java
@@ -14,73 +14,29 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.michaldrabik.tapbarmenulib.TapBarMenu;
+
 import districtdigital.agency.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MainFragment extends Fragment {
+    private TapBarMenu tapBarMenu;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
-        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        tapBarMenu = view.findViewById(R.id.tapBarMenu);
+        tapBarMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "Clicked a button!", Toast.LENGTH_SHORT).show();
+                tapBarMenu.toggle();
             }
         });
-        ViewPager pager = (ViewPager) view.findViewById(R.id.pager);
-        pager.setAdapter(new PagerAdapter());
+
+
         return view;
     }
 
-    private class PagerAdapter extends android.support.v4.view.PagerAdapter {
-
-        @Override
-        public int getCount() {
-            return 6;
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-
-            // Create some layout params
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.WRAP_CONTENT,
-                    RelativeLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-
-            // Create some text
-            TextView textView = getTextView(container.getContext());
-            textView.setText(String.valueOf(position));
-            textView.setLayoutParams(layoutParams);
-
-            RelativeLayout layout = new RelativeLayout(container.getContext());
-            layout.setBackgroundColor(ContextCompat.getColor(container.getContext(), R.color.colorPrimary));
-            layout.setLayoutParams(layoutParams);
-
-            layout.addView(textView);
-            container.addView(layout);
-            return layout;
-        }
-
-        private TextView getTextView(Context context) {
-            TextView textView = new TextView(context);
-            textView.setTextColor(Color.WHITE);
-            textView.setTextSize(30);
-            textView.setTypeface(Typeface.DEFAULT_BOLD);
-            return textView;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            container.removeView((RelativeLayout) object);
-        }
-    }
 }
